0ee26f76eed803fb6fb20f052caec12817bf1867
vitorsoer <contato.soer@gmail.com>
2023-02-22 20:33:30 -0300

    Repo config

---
+++ basic/types.py
@@ -0,0 +1,24 @@
+# Type: String
+string = 'Hello World'
+stringType: str = 'Some random text...'
+print(string, stringType)
+
+# Type: Number
+integer = 10
+integerType: int = 5
+print(integer, integerType)
+
+floatNumber = 12.5
+floatType: float = 50.75
+print(floatNumber, floatType)
+
+# Type: Boolean
+boolean = False
+booleanType: bool = True
+
+# Convert types
+print(
+    type(int('10')),
+    type(float('10.5')),
+    type(str(10))
+)
---
+++ basic/variables.py
@@ -0,0 +1,7 @@
+# Variables
+name = 'Bob'
+age = 20
+height = 1.75
+adult = age >= 18
+
+print('Name:', name, '|', 'Height:', height, '|', 'Adult:', adult)
